837f4fc16e83d4c10fa8a4c70d3ee7580e54e42b
SHAILESH <shaileshdubeyr@gmail.com>
2021-12-22 10:42:56 +0530

    Added : code for unique cuponcode generation

---
+++ UniqueCopan.php
@@ -0,0 +1,23 @@
+<?php
+function uniqueCoupanGeneration(){
+    $coupanLimit = readline("Enter the number :- ");
+    $array = array();
+    $counter = 0;
+    $duplicateValueInArray = 0;
+    while($counter != $coupanLimit){
+        $randomNumber = rand(0,50);
+        foreach($array as $value){
+            if($randomNumber == $value){
+                $duplicateValueInArray++;            //checking for duplicate value
+            }
+        }
+        if($duplicateValueInArray == 0){
+            $array[$counter] = $randomNumber;       //addind unique coupan code
+            $counter++;
+        }     
+        $duplicateValueInArray = 0;                       
+    }
+    var_dump($array);
+}
+uniqueCoupanGeneration();
+?>
